23cfaba066b6ad510a145cf3c91312fb8fddb169
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-11-04 19:16:23 -0500

    Service Interfaces - IRemoteMovieService

---
+++ Services/Interfaces/IRemoteMovieService.cs
@@ -0,0 +1,19 @@
+﻿using MoviePro.Enums;
+using MoviePro.Models.TMDB;
+using System.Threading.Tasks;
+
+namespace MoviePro.Services.Interfaces
+{
+    public interface IRemoteMovieService
+    {
+        // A query to the TMDB API. the ID is the TMDB movie api. Returns the MovieDetail object for the Movie. 
+        Task<MovieDetail> MovieDetailAsync(int id);
+
+        // A search to the TMDB API returns a MovieSearch object.
+        Task<MovieSearch> SearchMoviesAsync(MovieCategory category, int count);
+
+        // A query to the TMDB API. The Id is the TMDB actor Id. Returns the ActorDetail object for the Actor.  
+        Task<ActorDetail> ActorDetailAsync(int id);
+
+    }
+}
