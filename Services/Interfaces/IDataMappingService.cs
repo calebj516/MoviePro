257ef0487ec3ad9ba9f2ea4b5e04432b0115b824
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-11-04 19:23:10 -0500

    Service Interfaces - IDataMappingService
    The IDataMappingService interface will create three methods that any class implementing the interface must implement. These methods will be used to map the properties coming from the TMDB API query into our custom models.

---
+++ Services/Interfaces/IDataMappingService.cs
@@ -0,0 +1,18 @@
+﻿using MoviePro.Models.Database;
+using MoviePro.Models.TMDB;
+using System.Threading.Tasks;
+
+namespace MoviePro.Services.Interfaces
+{
+    public interface IDataMappingService
+    {
+        // When querying a movie in the TMDB API this method maps the properties coming from the TMDB API JSON to our Movie Model.
+        Task<Movie> MapMovieDetailAsync(MovieDetail movie);
+
+        // When querying an actor in the TMDB API this method maps the properties coming from the TMDB API JSON to our ActorDetail Model.
+        ActorDetail MapActorDetail(ActorDetail actor);
+
+        // Constructs a proper path to display cast Images.
+        string BuildCastImage(string profilePath);
+    }
+}
